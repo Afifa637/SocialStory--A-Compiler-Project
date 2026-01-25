03a0a4462539054a9d8710891a517c1daf4a3631
Afifa Sultana <afifasultana637@gmail.com>
2026-01-25 22:44:24 +0600

    Corrected Error detection

--- socialstory_tokens.h
+++ socialstory_tokens.h
@@ -2,11 +2,11 @@
 #define SOCIALSTORY_TOKENS_H
 
 enum TokenType {
-    // Program structure 
+    /* Program structure */
     T_GO_LIVE = 256,
     T_END_LIVE,
     
-    // Account & Entity keywords 
+    /* Account & Entity keywords */
     T_THE_ACCOUNT,
     T_WAS_CREATED,
     T_THE_POST,
@@ -16,8 +16,9 @@ enum TokenType {
     T_POSTED,
     T_ABOUT,
     T_THAT_HAD,
+    T_ANOTHER,
     
-    // Variable initialization verbs 
+    /* Variable initialization verbs */
     T_STARTED_WITH,
     T_BEGAN_AT,
     T_NOW_HAS,
@@ -25,20 +26,35 @@ enum TokenType {
     T_IS_VIRAL,
     T_IS_TRENDING,
     
-    // Update action verbs 
+    /* Action verbs (for operations) */
     T_GAINED,
     T_LOST,
+    T_ADDED,
+    T_REMOVED,
     T_INCREASED_BY,
     T_DECREASED_BY,
     T_WAS_UPDATED_TO,
     T_BECAME,
+    T_MULTIPLIED_BY,
+    T_DIVIDED_BY,
     
-    // Data structure keywords 
+    /* Arithmetic words (for calculations) */
+    T_PLUS,
+    T_MINUS,
+    T_TIMES,
+    T_MULTIPLY,
+    T_DIVIDED_EVENLY_BY,
+    T_DIVIDE,
+    T_MODULO,
+    T_WITH,
+    T_AND_THEN,
+    
+    /* Data structure keywords */
     T_CONTAINS,
     T_THE_FEED,
     T_AT_INDEX,
     
-    // Type keywords 
+    /* Type keywords */
     T_LIKES,
     T_FOLLOWERS,
     T_VIEWS,
@@ -50,7 +66,7 @@ enum TokenType {
     T_REACH,
     T_GROWTH_RATE,
     
-    // Conditional keywords 
+    /* Conditional keywords */
     T_WHEN,
     T_OTHERWISE_IF,
     T_OTHERWISE,
@@ -60,15 +76,17 @@ enum TokenType {
     T_DROPPED_BELOW,
     T_STAYED_AT,
     
-    // Comparison operators (narrative style) 
+    /* Comparison operators */
     T_MORE_THAN,
     T_LESS_THAN,
     T_EXACTLY,
     T_AT_LEAST,
     T_AT_MOST,
     T_DIFFERENT_FROM,
+    T_EQUAL_TO,
+    T_NOT_EQUAL_TO,
     
-    // Loop keywords 
+    /* Loop keywords */
     T_EVERY_DAY_FOR,
     T_EVERY,
     T_DAYS,
@@ -82,23 +100,23 @@ enum TokenType {
     T_TRENDING_LOOP,
     T_UNTIL,
     
-    // Loop control 
+    /* Loop control */
     T_STOP_THE_STORY,
     T_SKIP_THIS_POST,
     
-    // Function keywords 
+    /* Function keywords */
     T_THE_STORY_OF,
     T_BEGINS_WITH,
     T_TELL_BACK,
     T_THE_STORY_ENDS,
     T_TELL,
     
-    // I/O keywords 
+    /* I/O keywords */
     T_ANNOUNCE,
     T_ASK_FOR,
     T_DISPLAY,
     
-    // Built-in functions - Analytics 
+    /* Built-in functions */
     T_CALCULATE_VIRALITY,
     T_CALCULATE_ENGAGEMENT,
     T_FIND_TOP_POST,
@@ -108,26 +126,22 @@ enum TokenType {
     T_CHECK_IF_TRENDING,
     T_ANALYZE_GROWTH,
     T_FIND_MAX_VIRAL_ACCOUNT,
-    
-    // Built-in functions - String operations 
     T_REVERSE_THE_CAPTION,
     T_DETECT_SPAM,
-    
-    // Built-in functions - Utility
     T_CLAMP_ENGAGEMENT,
     T_BETWEEN,
     T_REVERSE_GROWTH,
     
-    // Boolean literals 
+    /* Boolean literals */
     T_TRUE_STORY,
     T_FALSE_ALARM,
     
-    // Logical operators 
-    T_ALSO,      // and 
-    T_EITHER,    // or 
-    T_OPPOSITE,  // not 
+    /* Logical operators */
+    T_ALSO,
+    T_EITHER,
+    T_OPPOSITE,
     
-    // Punctuation & Operators 
+    /* Punctuation */
     T_LPAREN,
     T_RPAREN,
     T_LBRACE,
@@ -137,38 +151,8 @@ enum TokenType {
     T_COMMA,
     T_DOT,
     T_COLON,
-    T_SEMICOLON,
     
-    // Arithmetic operators 
-    T_PLUS,
-    T_MINUS,
-    T_MULTIPLY,
-    T_DIVIDE,
-    T_PERCENT,
-    
-    // Assignment and compound operators 
-    T_EQUALS,
-    T_PLUS_EQUALS,
-    T_MINUS_EQUALS,
-    T_MULT_EQUALS,
-    T_DIV_EQUALS,
-    T_INCREMENT,
-    T_DECREMENT,
-    
-    // Relational operators (C-style) 
-    T_EQ,
-    T_NEQ,
-    T_LT,
-    T_GT,
-    T_LTE,
-    T_GTE,
-    
-    // Logical operators (C-style) 
-    T_AND,
-    T_OR,
-    T_NOT,
-    
-    // Literals & Identifiers 
+    /* Literals & Identifiers */
     T_ID,
     T_NUMBER,
     T_DECIMAL,
@@ -176,7 +160,6 @@ enum TokenType {
     T_CHAR
 };
 
- // Union for semantic values 
 typedef union {
     int    ival;
     double fval;
@@ -188,4 +171,4 @@ typedef union {
 extern YYSTYPE yylval;
 extern int yylineno;
 
-#endif 
+#endif
